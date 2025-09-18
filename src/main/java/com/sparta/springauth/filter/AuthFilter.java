a6c87519ffae2d1add6e0ba767c262b5c67e16be
ming <oterges00@gmail.com>
2025-09-18 14:18:46 +0900

    필터

---
+++ src/main/java/com/sparta/springauth/filter/AuthFilter.java
@@ -0,0 +1,71 @@
+package com.sparta.springauth.filter;
+
+import com.sparta.springauth.entity.User;
+import com.sparta.springauth.jwt.JwtUtil;
+import com.sparta.springauth.repository.UserRepository;
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import java.io.IOException;
+
+@Slf4j(topic = "AuthFilter")
+@Component
+@Order(2)
+public class AuthFilter implements Filter {
+
+    private final UserRepository userRepository;
+    private final JwtUtil jwtUtil;
+
+    public AuthFilter(UserRepository userRepository, JwtUtil jwtUtil) {
+        this.userRepository = userRepository;
+        this.jwtUtil = jwtUtil;
+    }
+
+    //DispatcherServlet보다 앞단이므로 @CookieValue로 쿠키를 가져올 수 없음
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        //getCookies(), getHeader(), getMethod() 같은 HTTP 전용 기능을 쓰기 위해 다운캐스팅
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        String url = httpServletRequest.getRequestURI();
+        //공백이 아닌지
+        if (StringUtils.hasText(url) &&
+                (url.startsWith("/api/user") || url.startsWith("/css") || url.startsWith("/js"))
+        ) {
+            log.info("인증 처리를 하지 않는 URL : " + url);
+            // 회원가입, 로그인 관련 API 는 인증 필요없이 요청 진행 (회원가입/로그인은 첫 단계이므로 사전 인증단계가 필요없음)
+            chain.doFilter(request, response); // 다음 Filter 로 이동
+        } else {
+            // 나머지 API 요청은 인증 처리 진행
+            // 토큰 확인
+            String tokenValue = jwtUtil.getTokenFromRequest(httpServletRequest); //우리가 찾는 쿠키 가져오기
+
+            if (StringUtils.hasText(tokenValue)) { // 토큰이 존재하면 검증 시작
+                // JWT 토큰 substring
+                String token = jwtUtil.substringToken(tokenValue);
+
+                // 토큰 검증
+                if (!jwtUtil.validateToken(token)) {
+                    throw new IllegalArgumentException("Token Error");
+                }
+
+                // 토큰에서 사용자 정보 가져오기
+                Claims info = jwtUtil.getUserInfoFromToken(token);
+
+                User user = userRepository.findByUsername(info.getSubject()).orElseThrow(() ->
+                        new NullPointerException("Not Found User")
+                );
+
+                request.setAttribute("user", user); //user객체를 컨트롤러에 보냄 (Request객체는 컨트롤러까지 넘어가므로 reuqest객체에 담아줌)
+                chain.doFilter(request, response); // 다음 Filter 로 이동
+            } else {
+                throw new IllegalArgumentException("Not Found Token");
+            }
+        }
+    }
+
+}
---
+++ src/main/java/com/sparta/springauth/filter/LoggingFilter.java
@@ -0,0 +1,28 @@
+package com.sparta.springauth.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Slf4j(topic = "LoggingFilter") //로깅 찍힐 때 이름
+@Component //컴포넌트로 로깅 등록
+@Order(1) //필터 순서 지정
+public class LoggingFilter implements Filter { //인증 및 인가 처리 필터
+    @Override //파라미터 - FilterChain : 필터를 이동할 때 사용
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        // 전처리
+        //request 에서 url 정보를 가져올 것임
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        String url = httpServletRequest.getRequestURI();
+        log.info(url); //어떤 요청인지 로그를 찍음
+
+        chain.doFilter(request, response); // 다음 Filter 로 이동
+
+        // 후처리
+        log.info("비즈니스 로직 완료");
+    }
+}
