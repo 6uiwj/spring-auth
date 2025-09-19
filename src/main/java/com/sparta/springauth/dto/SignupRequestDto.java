4de75f5846577f6fab7ce5c5b1b78d689669d635
ming <oterges00@gmail.com>
2025-09-19 14:25:48 +0900

    이메일 정규표현식

--- src/main/java/com/sparta/springauth/dto/SignupRequestDto.java
+++ src/main/java/com/sparta/springauth/dto/SignupRequestDto.java
@@ -2,6 +2,7 @@ package com.sparta.springauth.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,7 +13,8 @@ public class SignupRequestDto {
     private String username;
     @NotBlank
     private String password;
-    @Email
+
+    @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$")
     @NotBlank
     private String email;
     private boolean admin = false;
