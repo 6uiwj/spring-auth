fd4c8d63af5479b0cf096658c78c9fce80359a93
ming <oterges00@gmail.com>
2025-09-19 12:56:00 +0900

    Validation 예외처리

--- src/main/java/com/sparta/springauth/controller/UserController.java
+++ src/main/java/com/sparta/springauth/controller/UserController.java
@@ -3,23 +3,28 @@ package com.sparta.springauth.controller;
 import com.sparta.springauth.dto.LoginRequestDto;
 import com.sparta.springauth.dto.SignupRequestDto;
 import com.sparta.springauth.service.UserService;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
+@Slf4j
 @Controller
 @RequestMapping("/api")
 public class UserController {
 
+    private final UserService userService;
+
     public UserController(UserService userService) {
         this.userService = userService;
     }
-
-    private final UserService userService;
 
     @GetMapping("/user/login-page")
     public String loginPage() {
@@ -31,22 +36,22 @@ public class UserController {
         return "signup";
     }
 
+    //Validation에서 예외가 발생하면 BindingResult 객체에 오류에 대한 정보가 담김
     @PostMapping("/user/signup")
-    public String signup(SignupRequestDto requestDto) {
-        userService.signup(requestDto);
+    public String signup(@Valid SignupRequestDto requestDto, BindingResult bindingResult) {
+        // Validation 예외처리
+        List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+        if(fieldErrors.size() > 0) {
+            for (FieldError fieldError : bindingResult.getFieldErrors()) {
+                //오류가 난 필드 가져오기                         에러 메시지 출력
+                log.error(fieldError.getField() + " 필드 : " + fieldError.getDefaultMessage());
+            }
+            //오류가 발생했으므로 다시 회원가입 페이지로 리다이렉트
+            return "redirect:/api/user/signup";
+        }
 
+        userService.signup(requestDto);
+        //오류가 없이 회원가입에 성공했으니 로그인페이질 ㅗ이동
         return "redirect:/api/user/login-page";
     }
-
-    //이제 필터에서 처리할 것이므로 삭제
-//    @PostMapping("/user/login")
-//    public String login(LoginRequestDto requestDto, HttpServletResponse res) {
-//        try {
-//            userService.login(requestDto, res);
-//        } catch (Exception e) { //로그인 실패시 서버가 error페이지로 이동하라는 리다이렉트를 보내고, 클라이언트가 아래의 주소로 요청을 보냄
-//            return "redirect:/api/user/login-page?error";
-//        }
-//
-//        return "redirect:/";
-//    }
 }
